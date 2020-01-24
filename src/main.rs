f8ad78670ce5cad4634815de52dede6727e6f958
Julio Daniel Reyes <nemesiscodex@gmail.com>
2020-01-23 22:43:24 -0300

    First Service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate actix_web;
+extern crate actix_rt;
+
+use actix_web::{Responder, HttpServer, App};
+use std::io::Result;
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    format!("Hello World!")
+}
+
+
+#[actix_rt::main]
+async fn main() -> Result<()> {
+    println!("Starting server at http://127.0.0.1:8080");
+    HttpServer::new(|| App::new() 
+        .service(hello)
+    )
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
