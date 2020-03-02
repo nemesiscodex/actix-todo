8e596b4264f89925c537ff4c457549e9e91c3730
Julio Daniel Reyes <nemesiscodex@gmail.com>
2020-03-01 21:17:58 -0300

    Improve test cache (#6)

--- tests/test.rs
+++ tests/test.rs
@@ -8,16 +8,19 @@ use actix_todo::models::{AppState, TodoList};
 use actix_todo::handlers::todos;
 use tokio_postgres::NoTls;
 use slog::{info};
+use std::env;
 
 fn setup () -> AppState {
 
     dotenv().ok();
 
+    let log = Config::configure_log();
+
+    info!(log, "Server host: {:?}", env::var("SERVER.HOST").unwrap());
+
     let config = Config::from_env().unwrap();
 
     let pool = config.pg.create_pool(NoTls).unwrap();
-
-    let log = Config::configure_log();
 
     AppState {
         pool: pool.clone(),
@@ -35,14 +38,14 @@ async fn test_get_todos() {
 
     let app = App::new()
         .data(state)
-        .route("/", web::get().to(todos));
+        .route("/todos", web::get().to(todos));
 
     let mut app = test::init_service(app).await;
 
-    let req = test::TestRequest::get().uri("/").to_request();
+    let req = test::TestRequest::get().uri("/todos").to_request();
 
     let todos: Vec<TodoList> = test::read_response_json(&mut app, req).await;
 
-    info!(log, "{:?}", todos);
+    info!(log, "GET /todos -> {:?}", todos);
 
 }
